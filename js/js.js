ee1f0c32a078e500a75d2951ea5a14c9d42b7792
kkovalenko8 <152792216+kkovalenko8@users.noreply.github.com>
2023-12-04 20:57:06 +0100

    Create js.js

---
+++ js/js.js
@@ -0,0 +1,35 @@
+btn1.addEventListener("click", function () {
+    if (a3.checked) {
+        alert("Вірно! Так тримати!");
+    } else {
+        alert("Невірно! Спробуй ще.");
+    }
+});
+btn2.addEventListener("click", function () {
+    if (b4.checked) {
+        alert("Вірно! Так тримати!");
+    } else {
+        alert("Невірно! Спробуй ще.");
+    }
+});
+btn3.addEventListener("click", function () {
+    if (c2.checked) {
+        alert("Вірно! Так тримати!");
+    } else {
+        alert("Невірно! Спробуй ще.");
+    }
+});
+btn4.addEventListener("click", function () {
+    if (d2.checked) {
+        alert("Вірно! Так тримати!");
+    } else {
+        alert("Невірно! Спробуй ще.");
+    }
+});
+btn5.addEventListener("click", function () {
+    if (e2.checked) {
+        alert("Вірно! Так тримати!");
+    } else {
+        alert("Невірно! Спробуй ще.");
+    }
+});
